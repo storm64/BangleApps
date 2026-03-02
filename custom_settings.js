975d085bc60e8ba6f4632a06bf4e85fc1082ca46
storm64_PS1 <github@storm64.de>
2026-03-02 14:31:04 +0100

    Correct personal appstore changes

---
+++ custom_settings.js
@@ -0,0 +1,13 @@
+// Enforce custom settings to overwrite DEFAULTSETTINGS
+const CUSTOMSETTINGS = {
+  language : "de_DE",
+  sendUsageStats: false,  // send usage stats to banglejs.com
+  alwaysAllowUpdate : true, //  Always show "reinstall app" button regardless of the version
+  autoReload: true, //  Automatically reload watch after app App Loader actions (removes "Hold button" prompt))
+};
+window.addEventListener('load', function () {
+    if (DEFAULTSETTINGS == SETTINGS) {
+        SETTINGS = JSON.parse(JSON.stringify(CUSTOMSETTINGS)); // clone
+
+    }
+});
